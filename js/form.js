6d1c361db2edc06cbb03469264ee7ba8baa2708a
CMconsults <aderelemeshach@gmail.com>
2019-09-19 01:01:18 +0100

    All dashboard functions now working except delete button

--- js/form.js
+++ js/form.js
@@ -1,46 +1,58 @@
-$(document).ready(function() {
- 
-   //Add Patient Function
-  $('.regSubmitBtn').click(function(event) {
+$(document).ready(function () {
+
+  //Add Patient Function
+  $('.addSubmitBtn').click(function (event) {
     event.preventDefault();
-    const Surname = $('#fullname').val();
-    const username = $('#username').val();
-    const password = $('#password').val();
-    const email = $('#email').val();
+    const surname = $('#surname').val();
+    const othername = $('#othername').val();
+    const emailaddress = $('#emailaddress').val();
+    const dob = $('#dob').val();
+    const phonenumber = $('#phonenumber').val();
+    const presentaddress = $('#presentaddress').val();
+    const permanentaddress = $('#permanentaddress').val();
+    const cardnumber = $('#cardnumber').val();
     //Check if user input is empty
-    if (!fullname || !username || !password || !email) {
-      $('.regMessage').html('Kindly fill in all fields');
+
+    if (!surname || !othername || !emailaddress || !dob || !phonenumber || !presentaddress || !permanentaddress || !cardnumber) {
+      $('.addMessage').html('Kindly fill in all fields');
       return;
     }
+
     //Make get request to check if the user already exist
+
+
     $.ajax({
       method: 'GET',
-      url: `http://localhost:3000/users?email=${email}`,
+      url: `http://localhost:3000/patients?cardnumber=${cardnumber}`,
       data: {
-        email,
+        cardnumber,
       },
-      beforeSend: function() {
-        $('.regMessage').html('Loading....');
+      beforeSend: function () {
+        $('.addMessage').html('Loading....');
       },
-      success: function(response) {
+      success: function (response) {
         if (response.length) {
-          $('.regMessage').html('User already exist');
+          $('.addMessage').html('Patient already exist');
         } else {
           //Submit the user data if the user does not exist
           $.ajax({
             method: 'POST',
-            url: 'http://localhost:3000/users',
+            url: 'http://localhost:3000/patients',
             data: {
-              fullname,
-              username,
-              email,
-              password,
+              surname,
+              othername,
+              emailaddress,
+              dob,
+              phonenumber,
+              presentaddress,
+              permanentaddress,
+              cardnumber,
             },
-            beforeSend: function() {
-              $('.regMessage').html('Loading....');
+            beforeSend: function () {
+              $('.addMessage').html('Loading....');
             },
-            success: function() {
-              $('.regMessage').html('Registration Successfull');
+            success: function () {
+              $('.addMessage').html('Patient Added Successfully');
             },
           });
         }
@@ -48,10 +60,11 @@ $(document).ready(function() {
     });
   });
   //Login Function
-  $('.loginSubmitBtn').click(function(event) {
+  $('.loginSubmitBtn').click(function (event) {
     event.preventDefault();
     const passwordLogin = $('#passwordLogin').val();
     const emailLogin = $('#emailLogin').val();
+
     if (!passwordLogin || !emailLogin) {
       $('.regMessage').html('Kindly fill in all fields');
       return;
@@ -64,10 +77,10 @@ $(document).ready(function() {
         email: emailLogin,
         password: passwordLogin,
       },
-      beforeSend: function() {
+      beforeSend: function () {
         $('.regMessage').html('Loading....');
       },
-      success: function(response) {
+      success: function (response) {
         if (response.length) {
           $('.regMessage').html('Login sucessful');
           $('.checkLogin').html('You are logged in');
@@ -81,10 +94,184 @@ $(document).ready(function() {
     });
   });
   //Logout Function
-  $('.logoutBtn').click(function() {
+  $('.logoutBtn').click(function () {
     //clear the localstorage and redirect to signup page
     // localStorage.clear();
     // $('.checkLogin').html('Kindly login');
     window.location.assign('login.html');
   });
+
+  // Search Function
+
+
+
+  $('.searchForm').submit(function () {
+    event.preventDefault();
+    const searchInput = $('#searchInput').val();
+
+
+    if (!searchInput) {
+      $('.searchMessage').html('Kindly enter information');
+      return;
+    }
+
+    $.ajax({
+      method: 'GET',
+      url: `http://localhost:3000/patients`,
+
+      success: function (response) {
+
+
+        if (response.length) {
+          const userList = response.filter(function (res) {
+            return res.cardnumber === searchInput;
+          });
+          if (userList.length === 0) {
+            return $('.searchMessage').html('patient not found');
+          }
+          userList.forEach(element => {
+            $(".outputTable").prepend(`<tr>        
+           <td>${element.surname}</td>
+           <td>${element.othername}</td>
+           <td>${element.emailaddress}</td>
+           <td>${element.dob}</td>
+           <td>${element.phonenumber}</td>
+           <td><p data-placement="top" data-toggle="tooltip" title="View"><button class="btn btn-success btn-xs" data-title="view" data-toggle="modal" data-target="#view" data-cardNumber=${element.cardnumber} id=${element.id}><span class="glyphicon glyphicon-pencil">View</span></button></p></td>
+  
+           <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" data-cardNumber=${element.cardnumber} id=${element.id} ><span class="glyphicon glyphicon-pencil">Edit</span></button></p></td>
+  
+           <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" data-cardNumber=${element.cardnumber} id=${element.id}><span class="glyphicon glyphicon-trash">Delete</span></button></p></td>
+           </tr>
+           `);
+          });
+
+
+
+        }
+      },
+
+
+    });
+
+  });
+  $(document).on('click', "td button", function (e) {
+    e.preventDefault();
+    if ($(this).data('target') == '#edit') {
+
+      $.ajax({
+        method: 'GET',
+        url: `http://localhost:3000/patients?cardnumber=${$(this).data('cardnumber')}`,
+        success: function (res) {
+          console.log(res);
+          updateForm(res[0]);
+          // res[0]
+        }
+      });
+
+
+    } else if ($(this).data('target') == '#view') {
+      $.ajax({
+        method: 'GET',
+        url: `http://localhost:3000/patients?cardnumber=${$(this).data('cardnumber')}`,
+        success: function (res) {
+          console.log(res, 'view');
+          updateView(res[0]);
+        }
+      });
+    }
+
+  });
+  function updateView(data) {
+    $('#view div[name="cardnumber"]').text(data.cardnumber)
+    $('#view div[name="surname"]').text(data.surname)
+    $('#view div[name="othername"]').text(data.othername)
+    $('#view div[name="emailaddress"]').text(data.emailaddress)
+    $('#view div[name="dob"]').text(data.dob)
+    $('#view div[name="phonenumber"]').text(data.phonenumber)
+    $('#view div[name="presentaddress"]').text(data.presentaddress)
+    $('#view div[name="permanentaddress"]').text(data.permanentaddress)
+  }
+  // function updateForm() {
+
+  // }
+
+
+  // Show all the data in the database
+  $.ajax({
+    method: 'GET',
+    url: `http://localhost:3000/patients`,
+
+    success: function (response) {
+
+      response.forEach(element => {
+        $(".outputTableAll").prepend(`<tr>        
+         <td>${element.surname}</td>
+         <td>${element.othername}</td>
+         <td>${element.emailaddress}</td>
+         <td>${element.dob}</td>
+         <td>${element.phonenumber}</td>
+         <td><p data-placement="top" data-toggle="tooltip" title="View"><button class="btn btn-success btn-xs" data-title="view" data-toggle="modal" data-target="#view" data-cardNumber=${element.cardnumber} id=${element.id}><span class="glyphicon glyphicon-pencil">View</span></button></p></td>
+
+         <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" data-cardNumber=${element.cardnumber} id=${element.id} ><span class="glyphicon glyphicon-pencil">Edit</span></button></p></td>
+
+         <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete"  id=${element.id}><span class="glyphicon glyphicon-trash">Delete</span></button></p></td>
+         </tr>
+         `);
+      });
+
+
+
+
+    },
+
+
+  });
+  // Function to update for details
+  function updateForm(data) {
+    $("#edit").find('input[name="surname"]').val(data.surname);
+    $("#edit").find('input[name="othername"]').val(data.othername);
+    $("#edit").find('input[name="email"]').val(data.emailaddress);
+    $("#edit").find('input[name="dob"]').val(data.dob);
+    $("#edit").find('input[name="phonenumber"]').val(data.phonenumber);
+    $("#edit").find('input[name="presentaddress"]').val(data.presentaddress);
+    $("#edit").find('input[name="permanentaddress"]').val(data.permanentaddress);
+    $("#edit").find('button[name="update"]').attr('data-id', data.id);
+  }
+
+  $("#edit").find('button[name="update"]').click(function (e) {
+    e.preventDefault();
+    //   console.log('open')
+    //  console.log($('form[name="update"]'));
+    $.ajax({
+      url: `http://localhost:3000/patients/${$(this).data('id')}`,
+      method: 'PATCH',
+      data: $('form[name="update"]').serialize(), // It is used in form controls like <input>, <textarea>, <select> etc. It serializes the form values so that its serialized values can be used in the URL query string while making an AJAX request.
+      success: function (params) {
+        alert("Patient's information is successfully updated")
+      }
+    })
+  })
+
+  // Function for delete button
+  $(".delete-info").click(function (e) {
+    e.preventDefault();
+    $.ajax({
+      url: `http://localhost:3000/patients/${$(this).attr('id')}`,
+      method: 'DELETE',
+      success: function () {
+        alert("Patient's record has been deleted");
+      }
+    });
+  });
+
+
+  //Function for close button
+  $("#exit").click(function (e) {
+    e.preventDefault();
+    $(".modal-dialog").hide();
+    //   console.log('open')
+    //  console.log($('form[name="update"]'));
+
+  })
+
 });
